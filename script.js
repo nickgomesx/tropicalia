d74b92e28832fbfe72745f2662d70f5bbd9cffba
nicolly <159157320+nickgomesx@users.noreply.github.com>
2025-02-19 12:27:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentarFonteBotao = document.getElementById('aumentar-fonte');
+
+    let tamanhoAtualFonte =1;
+    aumentarFonteBotao.addEventListener('click', function (){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+     diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+   });
+});
